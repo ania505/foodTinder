3e70821d27f9ab594585826ec91ada0cc1c072d3
Anna Szymula <ania.szymula505@gmail.com>
2020-11-06 05:36:43 -0600

    google login frontend

--- client/components/Routes.js
+++ client/components/Routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import LoginPage from './LoginPage';
 
 
 export default class Routes extends React.Component {
@@ -15,7 +16,7 @@ export default class Routes extends React.Component {
                 <div>
                     <h2>Welcome to MeloMood!</h2>
                     <h4>MeloMood is the mood tracker that embraces your mood through music.</h4>
-                    <p>Log in to get started!</p>
+                    <LoginPage />
                     
                 </div>
             </Router>
